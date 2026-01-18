b60fc4aee0d655be9b581221825ff7f48b93836e
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-01-18 23:42:45 +0200

    Created the config file

---
+++ backend/core/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    EMBEDDINGS_MODEL_PATH: str = os.getenv("EMBEDDINGS_MODEL_PATH")
+    OCR_MODEL_PATH: str = os.getenv("OCR_MODEL_PATH")
+    YOLO_MODEL_PATH: str = os.getenv("YOLO_MODEL_PATH")
+
+    SQLITE_DB_PATH: str = os.getenv("SQLITE_DB_PATH")
+
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
+
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("JWT_REFRESH_TOKEN_EXPIRE_DAYS", 30))
+
+
+settings = Settings()
