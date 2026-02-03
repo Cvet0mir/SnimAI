b4e74c91e61d4ef44aa3f8d4d49501f50fdcadfb
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-02-03 15:36:03 +0300

    Created the dependecies needed for now

---
+++ backend/dependecies.py
@@ -0,0 +1,34 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from sqlalchemy.orm import Session
+
+from db.database import SessionLocal
+from core.security import decode_token
+from crud.user import UserCrud
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    try:
+        payload = decode_token(token)
+        user_id = int(payload.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = UserCrud.get_by_id(db, user_id)
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
+
