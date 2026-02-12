d81d9901dc68fff832ecd2275e2273fdf2a06703
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-02-12 09:59:52 +0200

    Made small changes to some of the schemas

--- backend/schemas/flashcard.py
+++ backend/schemas/flashcard.py
@@ -1,7 +1,7 @@
 from .orm_base import BaseORM
 
 class FlashcardBase(BaseORM):
-    note_id: int
+    session_id: int
     question: str
     answer: str
 
--- backend/schemas/summary.py
+++ backend/schemas/summary.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+from typing import Annotated
+from pydantic import Field
+
 from .orm_base import BaseORM
 
-class SessionOut(BaseORM):
-    note_id: int
-    summary_text: str
-    model_used: str
 
+class SummaryOut(BaseORM):
+    id: int
+    session_id: int
+    summary_text: str
+    used_model: Annotated[str, Field(max_length=100)]
+    created_at: datetime
