1a542f21bff7601eef62b76fd8d2e641131671e3
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-02-16 22:23:11 +0200

    Deleted the processing jobs model

--- backend/db/models/__init__.py
+++ backend/db/models/__init__.py
@@ -4,5 +4,4 @@ from .note import Note
 from .quiz import Quiz
 from .summary import Summary
 from .flashcard import Flashcard
-from .processing_job import ProcessingJob
 from .question import Question
