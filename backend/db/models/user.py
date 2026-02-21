817d8b3999c98cf8a76d10a43e7d6da25fcdb079
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-02-21 16:35:04 +0200

    Added 2 fields the user model in the db

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -2,10 +2,6 @@ from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base, str_100, datetime_tz
-from .session import Session
-from .note import Note
-
-
 class User(Base):
     __tablename__ = "users"
 
@@ -14,6 +10,9 @@ class User(Base):
     email: Mapped[str_100] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     created_at: Mapped[datetime_tz] = mapped_column(default=func.now())
+    current_streak: Mapped[int] = mapped_column(default=0)
+    last_active_date: Mapped[datetime_tz] = mapped_column(nullable=True)
 
-    sessions: Mapped[list[Session]] = relationship(cascade="all, delete-orphan")
-    notes: Mapped[list[Note]] = relationship(cascade="all, delete-orphan")
+    sessions: Mapped[list["Session"]] = relationship(cascade="all, delete-orphan")
+    notes: Mapped[list["Note"]] = relationship(cascade="all, delete-orphan")
+
